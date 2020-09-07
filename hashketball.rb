9f3c0ce1d907f1dba1b2f8e59bb216986caa9552
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-07 02:31:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,7 @@ def game_hash
           :steals => 2,
           :blocks => 7,
           :slam_dunks => 2},
-        {:player_name => "Bismak Biyombo",
+        {:player_name => "Bismack Biyombo",
           :number => 0,
           :shoe => 16,
           :points => 12,
