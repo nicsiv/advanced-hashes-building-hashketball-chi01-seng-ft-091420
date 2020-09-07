f1af6cdadd246311ead3062d36aebda94972eb20
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-09-07 02:27:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,7 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{:player_name =>
-        "Alan Anderson",
+      :players => [{:player_name => "Alan Anderson",
           :number => 0,
           :shoe => 16,
           :points => 22,
@@ -56,8 +55,7 @@ def game_hash
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
-      :players => [{:player_name =>
-        "Jeff Adrien",
+      :players => [{:player_name => "Jeff Adrien",
           :number => 4,
           :shoe => 18,
           :points => 10,
